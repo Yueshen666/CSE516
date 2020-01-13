669889a37abcfb1dd62766a09e12a846e39c22e1
Yueshen666 <clafifrank1122@163.com>
2020-01-13 13:06:18 -0800

    Update simple_pca.m

--- lab2_PCA/simple_pca.m
+++ lab2_PCA/simple_pca.m
@@ -1,1 +1,19 @@
-% this .m file contains your defined pca function.% The function takes the data matrix X and the number of components k to keep;% The output of the function should be the reduced data matrix Zfunction Z = simple_pca(X, k)  % your code here  % step 1. normalization    mu = mean(X)  X = X - mu    % step 2. derive covariance matrix  [ m, n ] = size(X)  C = 1/m * X' * X  % step 3. get U, S, V using eig  [ V, D, W] = eig(C)    % step 4. get reduced_U and Z  reduced_U = V(:, 1:k)  Z = X* reduced_U    % end of code end
+% this .m file contains your defined pca function.
+% The function takes the data matrix X and the number of components k to keep;
+% The output of the function should be the reduced data matrix Z
+
+function Z = simple_pca(X, k)
+
+  % your code here
+  
+  % step 1. normalization
+ 
+  % step 2. derive covariance matrix
+
+  % step 3. get V L V' using eig
+
+  % step 4. get reduced_U and Z
+  
+  % end of code 
+  
+end
