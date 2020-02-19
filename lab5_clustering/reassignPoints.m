593bc60d12d52cec904c4d2ea64817a1f08fd856
Yueshen666 <clafifrank1122@163.com>
2020-02-19 10:21:06 -0800

    Update reassignPoints.m

--- lab5_clustering/reassignPoints.m
+++ lab5_clustering/reassignPoints.m
@@ -1,1 +1,8 @@
-function indices = reassignPoints(X, centroids)  K = size(centroids, 1);  indices = zeros(size(X,1), 1);  m = size(X,1);  for i=1:m    k = 1;    min_dist = sum((X(i,:) - centroids(1,:)) .^ 2);    for j=2:K        dist = sum((X(i,:) - centroids(j,:)) .^ 2);        if(dist < min_dist)          min_dist = dist;          k = j;        end    end    indices(i) = k;  endend
+function indices = reassignPoints(X, centroids)
+  K = size(centroids, 1);
+  indices = zeros(size(X,1), 1);
+  m = size(X,1);
+
+  for i=1:m
+    % you code here
+  end
